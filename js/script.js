ef4e892ece0af96d4fa7eccf536bfc88d917c2fe
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2021-12-26 15:29:30 -0300

    adicionando primeiros elementos da logica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// script do jogo
+// para medir tamanho da tela e atualizar caso tenha mudança do tamanho da janela.
+
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo () {
+
+    altura = window.innerHeight //permite recuperar a altura da tela do navegador
+    largura = window.innerWidth //permite recuperar a largura da tela do navegador
+    
+    console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+//criando posições randômicas
+
+var posicaoX = Math.random()
+var posicaoY = Math.random()
+
+console.log(posicaoX, posicaoY)
